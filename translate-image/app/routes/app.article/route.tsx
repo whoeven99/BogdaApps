71b458cc374270b42e167bf03bdd30643be804f8
liuwenjie837 <15159210793@163.com>
2025-11-10 09:07:04 +0800

    #

--- translate-image/app/routes/app.article/route.tsx
+++ translate-image/app/routes/app.article/route.tsx
@@ -44,8 +44,10 @@ export const action = async ({ request }: ActionFunctionArgs) => {
                   image {
                     url
                     id
+                    altText
                   }
                   title
+                  isPublished
                 }
                 pageInfo {
                   endCursor
@@ -59,23 +61,24 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
           const response = await loadData.json();
           console.log("response:", response);
-          if (response.data.articles.nodes.length > 0) {
-            const data = response.data.articles.nodes.map((item: any) => item);
-            return {
+          const articles = response?.data?.articles;
+          if (articles?.nodes?.length > 0) {
+            const data = articles?.nodes?.map((item: any) => item);
+            return json({
               data,
-              endCursor: response.data.articles.pageInfo.endCursor,
-              hasNextPage: response.data.articles.pageInfo.hasNextPage,
-              hasPreviousPage: response.data.articles.pageInfo.hasPreviousPage,
-              startCursor: response.data.articles.pageInfo.startCursor,
-            };
+              endCursor: articles?.pageInfo?.endCursor,
+              hasNextPage: articles?.pageInfo?.hasNextPage,
+              hasPreviousPage: articles?.pageInfo?.hasPreviousPage,
+              startCursor: articles?.pageInfo?.startCursor,
+            });
           } else {
-            return {
+            return json({
               data: [],
               endCursor: "",
               hasNextPage: "",
               hasPreviousPage: "",
               startCursor: "",
-            };
+            });
           }
         } catch (error) {
           console.error("Error action loadData productImage:", error);
@@ -128,10 +131,110 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 };
 
 export default function Index() {
+  const languageFetcher = useFetcher<any>();
+  const productsFetcher = useFetcher<any>();
+  const imageFetcher = useFetcher<any>();
+
+  const [productsStartCursor, setProductsStartCursor] = useState("");
+  const [productsEndCursor, setProductsEndCursor] = useState("");
+  const [lastRequestCursor, setLastRequestCursor] = useState<any>(null);
+  const [productImageData, setProductImageData] = useState<any>([]);
+  const [tableDataLoading, setTableDataLoading] = useState(true);
+
+  const [articleData, setArticleData] = useState<any>([]);
+  const [selectedKey, setSelectedKey] = useState("");
+
+  const [productsHasNextPage, setProductsHasNextPage] = useState(false);
+  const [productsHasPreviousPage, setProductsHasPreviousPage] = useState(false);
+
+  const { t } = useTranslation();
+  const { Text } = Typography;
+  const navigate = useNavigate();
+
+  const [activeKey, setActiveKey] = useState("ALL");
+  const [searchText, setSearchText] = useState<string>("");
+  const timeoutIdRef = useRef<any>(true);
+
+  const [sortKey, setSortKey] = useState("CREATED_AT");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
   const loadFetcher = useFetcher<any>();
   const articleFetcher = useFetcher<any>();
-  const { t } = useTranslation();
   const [blogsData, setBlogsData] = useState<any[]>([]);
+  const panelColumns: ColumnsType<any> = [
+    {
+      // 不需要 dataIndex，用 render 直接取 item.node.title
+      width: 100,
+      render: (_: any, record: any) => {
+        // console.log("record", record);
+        return record.image ? (
+          <Thumbnail
+            source={record.image.url}
+            size="large"
+            alt={record.image.altText}
+          />
+        ) : (
+          <Thumbnail source={ImageIcon} size="large" alt="Small document" />
+        );
+      },
+      responsive: ["xs", "sm", "md", "lg", "xl", "xxl"], // ✅ 正确
+      onCell: () => ({
+        style: {
+          padding: "4px 8px", // ✅ 控制上下、左右 padding
+        },
+      }),
+    },
+    {
+      title: t("Article title"),
+      // 不需要 dataIndex，用 render 直接取 item.node.title
+      // maxWidth: 250, // ✅ 指定列宽（单位是像素）
+      render: (_: any, record: any) => (
+        <Text
+          className="hover-underline"
+          style={{
+            display: "inline-block",
+            maxWidth: 550, // 给内部留点空隙
+            whiteSpace: "normal", // ✅ 自动换行
+            wordBreak: "break-word", // ✅ 超长单词也能换行
+            lineHeight: 1.5,
+            fontSize: "14px",
+
+            // cursor:"pointer"
+          }}
+        >
+          {record?.title || "未命名产品"}
+        </Text>
+      ),
+      responsive: ["xs", "sm", "md", "lg", "xl", "xxl"], // ✅ 正确
+      onCell: () => ({
+        style: {
+          padding: "4px 8px", // ✅ 控制上下、左右 padding
+        },
+      }),
+    },
+    {
+      title: t("Status"),
+      width: 110,
+      render: (_: any, record: any) => {
+        if (record?.isPublished) {
+          return <Badge tone="success">{t("Published")}</Badge>;
+        }
+        return <Badge tone="attention">{t("UnPublished")}</Badge>;
+      },
+      responsive: ["md", "lg", "xl", "xxl"], // ✅ 手机端隐藏
+    },
+    {
+      title: t("Action"),
+      width: 150,
+      render: (_: any, record: any) => (
+        <Button onClick={() => handleView(record)}>{t("Manage")}</Button>
+      ),
+      responsive: ["md", "lg", "xl", "xxl"], // ✅ 手机端隐藏
+    },
+  ];
+  function handleView(record: any): void {
+    console.log("Viewing record:", record);
+    // navigate(`/app/products/${productId}`);
+  }
   useEffect(() => {
     const formData = new FormData();
     formData.append("loading", JSON.stringify({}));
@@ -139,8 +242,10 @@ export default function Index() {
   }, []);
   useEffect(() => {
     if (loadFetcher.data) {
-      console.log(loadFetcher.data);
-      setBlogsData(loadFetcher.data);
+      // console.log(loadFetcher.data);
+      setArticleData(loadFetcher.data?.data);
+      setTableDataLoading(false);
+      // setBlogsData(loadFetcher.data);
     }
   }, [loadFetcher.data]);
   useEffect(() => {
@@ -153,9 +258,126 @@ export default function Index() {
     formData.append("articleFetcher", JSON.stringify({ id }));
     articleFetcher.submit(formData, { method: "POST" });
   };
+  const handlePreProductPage = () => {
+    if (productsFetcher.state !== "idle") {
+      return;
+    }
+
+    productsFetcher.submit(
+      {
+        productStartCursor: JSON.stringify({
+          cursor: productsStartCursor,
+          query: searchText,
+          status: activeKey,
+          sortKey,
+          reverse: sortOrder === "asc" ? false : true,
+        }),
+      },
+      {
+        method: "post",
+        action: "/app/management",
+      },
+    ); // 提交表单请求
+  };
+  const handleNextProductPage = () => {
+    if (productsFetcher.state !== "idle") {
+      return;
+    }
+
+    productsFetcher.submit(
+      {
+        productEndCursor: JSON.stringify({
+          cursor: productsEndCursor,
+          query: searchText,
+          status: activeKey,
+          sortKey,
+          reverse: sortOrder === "asc" ? false : true,
+        }),
+      },
+      {
+        method: "post",
+        action: "/app/management",
+      },
+    ); // 提交表单请求
+  };
+
+  const handleChangeStatusTab = (key: string) => {
+    setActiveKey(key);
+    if (timeoutIdRef.current) {
+      clearTimeout(timeoutIdRef.current);
+    }
+
+    // 延迟 1s 再执行请求
+    timeoutIdRef.current = setTimeout(() => {
+      productsFetcher.submit(
+        {
+          productEndCursor: JSON.stringify({
+            cursor: "",
+            query: searchText,
+            status: key,
+            sortKey,
+            reverse: sortOrder === "asc" ? false : true,
+          }),
+        },
+        {
+          method: "post",
+          action: "/app/management",
+        },
+      );
+    }, 500);
+  };
+  const handleSearch = (value: string) => {
+    setSearchText(value);
+
+    // 清除上一次的定时器
+    if (timeoutIdRef.current) {
+      clearTimeout(timeoutIdRef.current);
+    }
+
+    // 延迟 1s 再执行请求
+    timeoutIdRef.current = setTimeout(() => {
+      productsFetcher.submit(
+        {
+          productEndCursor: JSON.stringify({
+            cursor: "",
+            query: value,
+            status: activeKey,
+            sortKey,
+            reverse: sortOrder === "asc" ? false : true,
+          }),
+        },
+        {
+          method: "post",
+          action: "/app/management",
+        },
+      );
+    }, 100);
+  };
+  const handleSortProduct = (key: string, order: "asc" | "desc") => {
+    setSortKey(key);
+    setSortOrder(order);
+    // 延迟 1s 再执行请求
+    timeoutIdRef.current = setTimeout(() => {
+      productsFetcher.submit(
+        {
+          productEndCursor: JSON.stringify({
+            cursor: "",
+            query: searchText,
+            sortKey: key,
+            status: activeKey,
+            reverse: order === "asc" ? false : true,
+          }),
+        },
+        {
+          method: "post",
+          action: "/app/management",
+        },
+      );
+    }, 100);
+  };
   return (
     <Page>
-      {blogsData.length > 0 &&
+      {/* {blogsData.length > 0 &&
         blogsData?.map((item) => {
           return (
             <Flex key={item.id} vertical gap={8}>
@@ -165,7 +387,100 @@ export default function Index() {
               </Button>
             </Flex>
           );
-        })}
+        })} */}
+      <Card styles={{ body: { padding: "12px 24px" } }}>
+        <Flex align="center" justify="space-between">
+          <Tabs
+            activeKey={activeKey}
+            onChange={(key) => handleChangeStatusTab(key)}
+            defaultActiveKey="all"
+            type="line"
+            style={{ width: "40%" }}
+            items={[
+              { label: t("All"), key: "ALL" },
+              {
+                label: t("Active"),
+                key: "ACTIVE",
+              },
+              {
+                label: t("Draft"),
+                key: "DRAFT",
+              },
+              {
+                label: t("Archived"),
+                key: "ARCHIVED",
+              },
+            ]}
+          />
+
+          <Flex align="center" justify="center" gap={20}>
+            <Input
+              placeholder={t("Search...")}
+              value={searchText}
+              onChange={(e) => handleSearch(e.target.value)}
+              prefix={<SearchOutlined />}
+            />
+            <SortPopover
+              onChange={(key, order) => handleSortProduct(key, order)}
+              sortKeyProp={sortKey}
+              sortOrderProp={sortOrder}
+            />
+            {/* <Button onClick={() => console.log(lastPageCursorInfo)}>
+                          输出store存储数据
+                        </Button> */}
+          </Flex>
+        </Flex>
+      </Card>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          // height: "calc(100vh)", // 根据页面结构调整
+          background: "#fff",
+          flex: "1",
+        }}
+      >
+        {/* 表格主体区域（可滚动） */}
+        <div style={{ flex: 1, overflow: "auto" }}>
+          <Table
+            dataSource={articleData}
+            columns={panelColumns}
+            pagination={false}
+            rowKey={(record) => record.key} // ✅ 建议加上 key，避免警告
+            loading={tableDataLoading}
+            onRow={(record) => ({
+              onClick: (e) => {
+                // 排除点击按钮等交互元素
+                if ((e.target as HTMLElement).closest("button")) return;
+                const productId = record.key.split("/").pop();
+                navigate(`/app/products/${productId}`);
+              },
+              style: { cursor: "pointer" },
+            })}
+          />
+        </div>
+
+        {/* 分页条固定在底部 */}
+        <div
+          style={{
+            borderTop: "1px solid #f0f0f0",
+            padding: "8px 0",
+            background: "#fff",
+            position: "sticky",
+            bottom: 0,
+            zIndex: 10,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Pagination
+            hasPrevious={productsHasPreviousPage}
+            onPrevious={handlePreProductPage}
+            hasNext={productsHasNextPage}
+            onNext={handleNextProductPage}
+          />
+        </div>
+      </div>
     </Page>
   );
 }
