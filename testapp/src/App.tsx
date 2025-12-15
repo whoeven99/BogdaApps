3fd896fbd541dd7fc2f5915e655af4f28edaa325
whoeven99 <whoeven99@outlook.com>
2025-12-16 00:54:02 +0800

    Init Monitor App

---
+++ testapp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to TestApp11</h1>
+      <p>This is a TypeScript React template.</p>
+    </div>
+  );
+};
+
+export default App;
