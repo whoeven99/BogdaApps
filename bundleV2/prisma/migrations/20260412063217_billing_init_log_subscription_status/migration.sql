4b9a82262e447032251aa7b0b5e9300af7263fce
whoeven99 <whoeven99@outlook.com>
2026-04-13 13:03:42 +0800

    Update migration

--- bundleV2/prisma/migrations/20260412063217_billing_init_log_subscription_status/migration.sql
+++ bundleV2/prisma/migrations/20260412063217_billing_init_log_subscription_status/migration.sql
@@ -1,17 +1,7 @@
-/*
-  Warnings:
 
-  - You are about to drop the `BillingSubscriptionWebhookLog` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "BillingInitLog" ADD COLUMN "subscriptionStatus" TEXT;
 ALTER TABLE "BillingInitLog" ADD COLUMN "subscriptionStatusUpdatedAt" DATETIME;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "BillingSubscriptionWebhookLog";
-PRAGMA foreign_keys=on;
 
 -- CreateIndex
 CREATE INDEX "BillingInitLog_shopifySubscriptionId_idx" ON "BillingInitLog"("shopifySubscriptionId");
