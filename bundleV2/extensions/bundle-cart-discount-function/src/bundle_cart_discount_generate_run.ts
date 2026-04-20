31f249fae0d64409a4c976dcffd7e0f2b2db742b
Daoyeee <156064834+Daoyeee@users.noreply.github.com>
2026-04-20 09:35:19 +0800

    1，修改购物车加购逻辑
    1，修改购物车加购逻辑

--- bundleV2/extensions/bundle-cart-discount-function/src/bundle_cart_discount_generate_run.ts
+++ bundleV2/extensions/bundle-cart-discount-function/src/bundle_cart_discount_generate_run.ts
@@ -358,6 +358,12 @@ const findOffer = (
       continue;
     }
 
+    const discountTiers = parseDiscountRulesJson(offer.discountRulesJson);
+    if (discountTiers.length === 0) {
+      log("offer_skip_no_rules", { offerId: offer.id, name: offer.name });
+      continue;
+    }
+
     const selectedIds = parseSelectedIds(offer.selectedProductsJson);
     if (!selectedIds.length) {
       log("offer_match_all_products", { offerId: offer.id, name: offer.name });
